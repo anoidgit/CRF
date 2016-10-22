3cd612a984d7cfb4af34bdbdeaf5a4437a1f8d74
Yanzi <jinyanzi1990@gmail.com>
2016-10-22 18:17:28 -0500

    function added

--- scripts/p3.sh
+++ scripts/p3.sh
@@ -2,8 +2,14 @@
 
 lambda="1e-4"
 
+if [[ -f tmp.txt ]];then
+	rm tmp.txt
+fi
+
 plot_setup="set terminal postscript font ',28'; set style fill pattern border; set xlabel 'CPU time (seconds)'; set yrange [0:*]; set lmargin 5; set tmargin 0; set bmargin 1;set rmargin 0;"
 
-echo $plot_setup" set ylabel 'Letter-wise error'; set output '| ps2pdf - p3_letter_err.pdf; plot '"$lambda".txt' using 6:xtic(4) title 'Training', using 8:xtic(1) title '$Testing';" | gnuplot
-echo $plot_setup" set ylabel 'Word-wise error'; set output '| ps2pdf - p3_word_err.pdf; plot '"$lambda".txt' using 7:xtic(4) title 'Training', using 9:xtic(1) title '$Testing';" | gnuplot
+awk '$1 ~ /^[0-9]+$/ {print $0}'  $lambda.txt > tmp.txt
+
+echo $plot_setup" set ylabel 'Letter-wise error'; set output '| ps2pdf - p3_letter_err.pdf; plot 'tmp.txt' using 6:xtic(4) title 'Training', using 8:xtic(1) title '$Testing';" | gnuplot
+echo $plot_setup" set ylabel 'Word-wise error'; set output '| ps2pdf - p3_word_err.pdf; plot 'tmp.txt' using 7:xtic(4) title 'Training', using 9:xtic(1) title '$Testing';" | gnuplot
 
--- scripts/p4.sh
+++ scripts/p4.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-lambda="1e-4"
 cores=("1" "2" "4" "6" "8")
 
 #rm *.txt
 
-plot_setup="set terminal postscript font ',28'; set style fill pattern border; set xlabel 'CPU time (seconds)'; set yrange[0:*];set ylabel 'object value'; set lmargin 5; set tmargin 0; set bmargin 1;set rmargin 0;"
 
-for lambda in ${lambdas[@]};do
-	echo "lambda = "$lambda
-#	mpirun -n 3 ../code_PETSc/seq_train -data ../data/train.txt -tdata ../data/test.txt -lambda $lambda -loss CRF -tol 1e-3 > $lambda.txt
-	awk '$1 ~ /^[0-9]+$/ {print $0}'  $lambda.txt > tmp.txt
-	echo $plot_setup" set output '| ps2pdf - p2_"$lambda".pdf; plot 'tmp.txt' using 1:xtic(4) notitle;" | gnuplot
-done
+function run_exp() {
+	lambda="1e-4"
+	tol="1e-3"
+	
+	for n in ${cores[@]};do
+		echo "use "$n" cores"
+		if [[ $n == "8" ]];then 
+			echo $n
+			tol="1e-5"
+		fi
+		echo "$PETSC_DIR/$PETSC_ARCH/bin/mpirun -n $n ../code_PETSc/seq_train -data ../data/train.txt -tdata ../data/test.txt -lambda $lambda -loss CRF -tol $tol > "core_"$n.txt"
+		#$PETSC_DIR/$PETSC_ARCH/bin/mpirun -n $n ../code_PETSc/seq_train -data ../data/train.txt -tdata ../data/test.txt -lambda $lambda -loss CRF -tol $tol > "core_"$n.txt
+	done
+}
 
-#rm *.txt
+function plot() {
+	plot_setup="set terminal postscript font ',28'; set style fill pattern border; set xlabel 'CPU time (seconds)'; set yrange[0:*];set ylabel 'object value'; set lmargin 5; set tmargin 0; set bmargin 1;set rmargin 0;"
+	for n in ${cores[@]};do
+		echo $n" cores"
+		awk '$1 ~ /^[0-9]+$/ {print $0}'  "core_"$n > tmp.txt
+		echo $plot_setup" set output '| ps2pdf - p4_scalability.pdf; plot 'tmp.txt' using 1:xtic(4) notitle;" | gnuplot
+	done
+	rm tmp.txt
+}
+
+run_exp
+
